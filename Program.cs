c34fe888ad02dcdd54b429841ac4be573ce5a0cd
OlegSs <youremail@yourdomain.comolegs934@gmail.com>
2022-10-11 13:08:04 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int second = a/10;
+int b = second%10;
+
+Console.WriteLine(b);
